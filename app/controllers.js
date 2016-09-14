730bcd2ca047a18781876c796f169ab301218887
Remya S <remya@artleafgallery.com>
2016-09-14 19:08:35 +0530

    color grid

--- app/controllers.js
+++ app/controllers.js
@@ -27,10 +27,11 @@ app.controller("ProductsController", function($scope,$http, productService, $sta
 	$scope.productId = $stateParams.productId;
 	$scope.categoryId = $stateParams.categoryId;
 	if($scope.categoryId) {
-		$scope.url = appConstants.apiUrl+'api/artworks/'+$scope.productId+"/?page=1";
+		$scope.url = appConstants.apiUrl+'api/artworks/'+$scope.productId+$scope.categoryId+"/?page=1";
 	} else {
 		$scope.url = appConstants.apiUrl+'api/artworks/'+$scope.productId+"/?page=1";
 	}
+	$scope.baseUrl = $scope.url;
 	$scope.previous = "";
 	$scope.artImages_slice = [];
 	$scope.page_number = 1;
@@ -39,28 +40,35 @@ app.controller("ProductsController", function($scope,$http, productService, $sta
 		'col2': [],
 		'col3': []
 	};
-	
+	$scope.no_result = false;
+	$scope.color = "";
 	$scope.getProductDetails = function(){
 		$http({
 		  method: 'GET',
 		  url: appConstants.apiUrl+'api/products/'+$scope.productId+"/"
 		}).then(function successCallback(response) {
 			$scope.product = response.data;
+			console.log($scope.product);
 		}, function errorCallback(response) {
 		    // called asynchronously if an error occurs
 		    // or server returns response with an error status.
 		});
 	}
 	$scope.getArtImages = function(){
-		console.log(typeof($scope.url));
+
 		if($scope.url != $scope.previous && $scope.url != null){	
 			$scope.previous = $scope.url;
 			$http({
 			  method: 'GET',
 			  url: $scope.url,
 			}).then(function successCallback(response) {
-				$scope.artImages = response.data.results;					
-				$scope.url = response.data.next;		
+				if(response.data.results.length == 0)
+					$scope.no_result = true;
+				else
+					$scope.no_result = false;
+				$scope.artImages = response.data.results;
+				if(response.data.next)					
+					$scope.url = response.data.next;		
 				$.each($scope.artImages, function(index, item){
 					if((index+1)%3 == 0){
 						$scope.page['col3'].push(item);
@@ -77,8 +85,46 @@ app.controller("ProductsController", function($scope,$http, productService, $sta
 			});
 		}
 	}
+	$scope.getArtImagesByColor = function(color){
+		$scope.page = {
+			'col1': [],
+			'col2': [],
+			'col3': []
+		};
+		var url = $scope.baseUrl + "&color=" + color.substr(1);
+		$scope.color = color;
+
+		$scope.previous = $scope.url;
+		$http({
+		  method: 'GET',
+		  url: url,
+		}).then(function successCallback(response) {
+			if(response.data.results.length == 0)
+				$scope.no_result = true;
+			else
+				$scope.no_result = false;
+			$scope.artImages = response.data.results;
+			if(response.data.next)					
+				$scope.url = response.data.next;		
+			$.each($scope.artImages, function(index, item){
+				if((index+1)%3 == 0){
+					$scope.page['col3'].push(item);
+				} else if((index+1)%2 == 0){
+					$scope.page['col2'].push(item);
+				} else {
+					$scope.page['col1'].push(item);
+				}
+			});
+			$scope.page_number = $scope.page_number + 1;
+		}, function errorCallback(response) {
+		    // called asynchronously if an error occurs
+		    // or server returns response with an error status.
+		});
+		
+	}
 	$scope.getArtImages();
 	$scope.getProductDetails();
+	
 });
 
 app.controller("HomeController", function($scope,$http, productService, appConstants){
